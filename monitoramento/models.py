d1dadc826c846a0f704a839f242149d56f4e0b07
Luiz Budeu <lugui@usp.br>
2022-10-07 15:59:39 -0300

    models migration Co-authored-by: Felipe Arrais <FelipeArrais@users.noreply.github.com> Co-authored-by: GaStephano-USP <GaStephano-USP@users.noreply.github.com>

--- monitoramento/models.py
+++ monitoramento/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
 # Create your models here.
+class Voos(models.Model):
+    STATUS_CHOICES = (
+        ('embarcando', 'embarcando'), 
+        ('cancelado', 'cancelado'), 
+        ('programado', 'programado'), 
+        ('taxiando', 'taxiando'), 
+        ('pronto', 'pronto'), 
+        ('autorizado', 'autorizado'), 
+        ('em voo', 'em voo'), 
+        ('aterrissado', 'aterrissado'),
+    )
+
+    companhia_aerea = models.CharField(max_length=100, blank=False)
+    codigo = models.CharField(max_length=6)  # codigo precisa ser 2 letras e 4 números (XX1111)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+    origem = models.CharField(max_length=100, blank=False)
+    destino = models.CharField(max_length=100, blank=False)
+    partida_prevista = models.DateTimeField(blank=False)
+    chegada_prevista = models.DateTimeField(blank=False)
+    partida_real = models.DateTimeField()
+    chegada_real = models.DateTimeField()
+
+    class Meta:
+        db_table = 'voos'
+
+
+class Partidas(models.Model):
+    STATUS_CHOICES = (
+        ('embarcando', 'embarcando'), 
+        ('cancelado', 'cancelado'), 
+        ('programado', 'programado'), 
+        ('taxiando', 'taxiando'), 
+        ('pronto', 'pronto'), 
+        ('autorizado', 'autorizado'), 
+        ('em voo', 'em voo'), 
+        ('aterrissado', 'aterrissado'),
+    )
+
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+    destino = models.CharField(max_length=100, blank=False)
+    partida_prevista = models.DateTimeField(blank=False)
+    partida_real = models.DateTimeField()
+
+    class Meta:
+        db_table = 'partidas'
+
+
+class Chegadas(models.Model):
+    origem = models.CharField(max_length=100, blank=False)
+    chegada_prevista = models.DateTimeField(blank=False)
+    chegada_real = models.DateTimeField()
+
+    class Meta:
+        db_table = 'chegadas'
