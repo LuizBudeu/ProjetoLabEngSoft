6283d637666dd8a841c75bda9c3ecc4003e41a4c
Luiz Budeu <lugui@usp.br>
2022-10-15 00:01:46 -0300

    finishing permissions, minor visual improvements

--- monitoramento/views.py
+++ monitoramento/views.py
@@ -28,6 +28,7 @@ def login(request):
         username = request.POST['username']
         password = request.POST['password']
         if username in logins and password == logins[username]['password']:
+            request.session['permission'] = logins[username]['permission']
             return redirect(f"home")
         else:
             context['error_msg'] = 'Usuário ou senha errados.'
@@ -35,30 +36,35 @@ def login(request):
     return render(request, 'login.html', context)
 
 
-def home(request, permission='None'):
-    context = {'permission': permission}
+def home(request):
+    context = {'permission': request.session.get('permission')}
     return render(request, 'home.html', context)
 
 
-def crud(request, permission='all'):
-    context = {'permission': permission}
+def crud(request):
+    context = {'permission': request.session.get('permission')}
     return render(request, 'crud.html', context)
+
 
 def crudcreate(request):
     context = {}
     return render(request, 'crud-create.html', context)
 
+
 def crudread(request):
     context = {}
     return render(request, 'crud-read.html', context)
+
 
 def crudupdate(request):
     context = {}
     return render(request, 'crud-update.html', context)
 
+
 def cruddelete(request):
     context = {}
     return render(request, 'crud-delete.html', context)
+
 
 def monitoramento(request):
     context = {}
@@ -69,9 +75,11 @@ def relatorio(request):
     context = {}
     return render(request, 'relatorio.html', context)
 
+
 def estado(request):
     context = {}
     return render(request, 'estado.html', context)
+
 
 def mostrarelatorio(request):
     context = {}
